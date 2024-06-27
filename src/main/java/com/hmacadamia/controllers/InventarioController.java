c05aa40e99b438d90e4ba003817290c2b144aa2f
Nicolas <nicolas.betancur759@pascualbravo.edu.co>
2024-06-27 11:19:15 -0500

    TabInventario

--- src/main/java/com/hmacadamia/controllers/InventarioController.java
+++ src/main/java/com/hmacadamia/controllers/InventarioController.java
@@ -1,6 +1,21 @@
 package com.hmacadamia.controllers;
 import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
 import javafx.scene.control.TabPane;
-public class InventarioController {
+import javafx.scene.control.TextField;
+import javafx.scene.layout.GridPane;
 
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class InventarioController implements Initializable {
+    @FXML
+    private GridPane gridInventario;
+    @FXML
+    private TextField txtSearchInventario;
+
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+
+    }
 }
